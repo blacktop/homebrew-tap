b70887bf572cd440cfa1959a8e2746e619693e67
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-09 20:59:07 -0600

    graboid version 0.1.0

---
+++ Formula/graboid.rb
@@ -0,0 +1,15 @@
+class Graboid < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/blacktop/graboid/releases/download/0.1.0/graboid_0.1.0_macOS_amd64.tar.gz"
+  version "0.1.0"
+  sha256 "49b1b3b9be0130cc7fa0be8da14e51a7ad825ef75787a4464838bc184c6e0c8b"
+
+  def install
+    bin.install "graboid"
+  end
+
+  test do
+    
+  end
+end
