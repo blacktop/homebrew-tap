1e9c3f39ffe36b464b4cc2ee1d25b91325073997
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-13 21:52:22 -0600

    graboid version 0.8.0

--- Formula/graboid.rb
+++ Formula/graboid.rb
@@ -1,9 +1,9 @@
 class Graboid < Formula
   desc "Clientless docker image downloader."
   homepage "https://github.com/blacktop/graboid"
-  url "https://github.com/blacktop/graboid/releases/download/0.7.0/graboid_0.7.0_macOS_amd64.tar.gz"
-  version "0.7.0"
-  sha256 "2d7f03918c2d096ba50abb2a904dade7f07348e03b4ad3dc1d1c9643fdde2e68"
+  url "https://github.com/blacktop/graboid/releases/download/0.8.0/graboid_0.8.0_macOS_amd64.tar.gz"
+  version "0.8.0"
+  sha256 "ebe02d789ddb32005917ae707e405345404aefcc8c27a831d7c965784f2eec25"
 
   def install
     bin.install "graboid"
