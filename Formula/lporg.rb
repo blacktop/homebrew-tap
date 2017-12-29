11bf01504a44a5f72a787b53ca3e6f942c468fa6
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-28 22:45:29 -0700

    lporg version 17.12.9

--- Formula/lporg.rb
+++ Formula/lporg.rb
@@ -1,9 +1,9 @@
 class Lporg < Formula
   desc "Organize Your macOS Launchpad Apps."
   homepage "https://github.com/blacktop/lporg"
-  url "https://github.com/blacktop/lporg/releases/download/17.12.8/lporg_17.12.8_macOS_amd64.tar.gz"
-  version "17.12.8"
-  sha256 "768be1aace1d0d61a77c9cd05d90727b89a8eb1608f832e10234a62aa84532df"
+  url "https://github.com/blacktop/lporg/releases/download/17.12.9/lporg_17.12.9_macOS_amd64.tar.gz"
+  version "17.12.9"
+  sha256 "589ab4df70b4daf5a3c2b8464e48b92f3f31d7bc02a7e2905a7f227b12863013"
 
   def install
     bin.install "lporg"
