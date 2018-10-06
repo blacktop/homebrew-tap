81cf4b56fb3f4123ea0702a3fbc7c5aeb9ea0732
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-06 12:28:30 -0400

    Brew formula update for get-ipsws version v18.09.7

--- Formula/get-ipsws.rb
+++ Formula/get-ipsws.rb
@@ -1,9 +1,9 @@
 class GetIpsws < Formula
   desc "Download ipsw(s) from ipsw.me or theiphonewiki.com"
   homepage "https://github.com/blacktop/get-ipsws"
-  url "https://github.com/blacktop/get-ipsws/releases/download/v18.09.6/get-ipsws_18.09.6_macOS_amd64.tar.gz"
-  version "18.09.6"
-  sha256 "5b962a0b0bd6a605e726c9e6947967ec940016e68561637bf9630849137ad47f"
+  url "https://github.com/blacktop/get-ipsws/releases/download/v18.09.7/get-ipsws_18.09.7_macOS_amd64.tar.gz"
+  version "18.09.7"
+  sha256 "ddc3d67938546d8665b1bab21d576f9ae3956a14519a11218869abfe984f429d"
 
   def install
     bin.install "get-ipsws"
