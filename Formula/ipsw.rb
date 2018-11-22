4a73791d5efb6f11198e22efa4b6f15f245ca921
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-21 22:17:01 -0500

    Brew formula update for ipsw version v18.11.1

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -1,9 +1,9 @@
 class Ipsw < Formula
   desc "Download ipsw(s) from ipsw.me or theiphonewiki.com"
   homepage "https://github.com/blacktop/ipsw"
-  url "https://github.com/blacktop/ipsw/releases/download/v18.10.2/ipsw_18.10.2_macOS_amd64.tar.gz"
-  version "18.10.2"
-  sha256 "c10ad88e35d265e0190583fc93bbd531c88d5dd155a36b516c69a214286dcad7"
+  url "https://github.com/blacktop/ipsw/releases/download/v18.11.1/ipsw_18.11.1_macOS_amd64.tar.gz"
+  version "18.11.1"
+  sha256 "a570dd3b8bece7d52ebf5e381ac0062c795fd62a448548b195e24720c1dbadc6"
 
   def install
     bin.install "ipsw"
