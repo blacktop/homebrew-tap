8b163571c9d4670043347c307d24cb6c71eaa266
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-01 21:24:36 -0500

    Brew formula update for seccomp-gen version v1.0.2

--- Formula/seccomp-gen.rb
+++ Formula/seccomp-gen.rb
@@ -1,9 +1,9 @@
 class SeccompGen < Formula
   desc "Docker Secure Computing Profile Generator."
   homepage "https://github.com/blacktop/seccomp-gen"
-  url "https://github.com/blacktop/seccomp-gen/releases/download/v1.0.1/seccomp-gen_1.0.1_macOS_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "670aac46b77a48533b8cfa19f11cb2b07416d79d1745693ed0efff7894a86c0d"
+  url "https://github.com/blacktop/seccomp-gen/releases/download/v1.0.2/seccomp-gen_1.0.2_macOS_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "6d16a0f7ccd12f81dfc5ab6dd77720f0f1d48e738feec6ca872a9ee3c5e4133b"
 
   def install
     bin.install "scgen"
