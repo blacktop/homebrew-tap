8614eac6d775448d4966b8c7ab08f2bea0124a93
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-15 22:14:42 -0400

    Brew formula update for ipsw version v20.04.1

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -2,12 +2,12 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "19.08.108"
+  version "20.04.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/blacktop/ipsw/releases/download/v19.08.108/ipsw_19.08.108_macOS_amd64_extras.tar.gz"
-    sha256 "1291aaaafc4077fac87cda75188edd930b44ee74cf28bce892d1c04a03a2e0a2"
+    url "https://github.com/blacktop/ipsw/releases/download/v20.04.1/ipsw_20.04.1_macOS_amd64_extras.tar.gz"
+    sha256 "62d02cc116cae7592c76c2b5ccd9408cdda9c0017b1fbad1eb0fdba159d67d67"
   elsif OS.linux?
   end
 
