157c1327b7d654c24121471aafa6a7d5cbd45ff9
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-25 21:56:27 -0500

    Brew formula update for ipsw version v20.08.89

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,24 +5,24 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "20.08.88"
+  version "20.08.89"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/blacktop/ipsw/releases/download/v20.08.88/ipsw_20.08.88_macOS_x86_64.tar.gz"
-    sha256 "ed0d9dfe3b02f5c4ded1c6e50473ea2aaeb91a622152ae185345a12a33dd3dcf"
+    url "https://github.com/blacktop/ipsw/releases/download/v20.08.89/ipsw_20.08.89_macOS_x86_64.tar.gz"
+    sha256 "c398a5e7dba9998e5ed65909f40f4309ad2f4ee618eeb11d8f1f8a830ba36594"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/blacktop/ipsw/releases/download/v20.08.88/ipsw_20.08.88_macOS_arm64.tar.gz"
-    sha256 "14b5694354dea3fb87e2df705b1d3bee0a5ab6dfd972ec9a92392bb4c9960632"
+    url "https://github.com/blacktop/ipsw/releases/download/v20.08.89/ipsw_20.08.89_macOS_arm64.tar.gz"
+    sha256 "c01a767e6bad340b57be809268acb41e9d9c3e2e6554ef879d37cfa4efe7f0d3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/blacktop/ipsw/releases/download/v20.08.88/ipsw_20.08.88_linux_x86_64.tar.gz"
-    sha256 "fb130f988964422f39bcfc376550118a23c4d2bed7f8189683dbd3ef35cbd4ee"
+    url "https://github.com/blacktop/ipsw/releases/download/v20.08.89/ipsw_20.08.89_linux_x86_64.tar.gz"
+    sha256 "2e0def6a7474938776674ccad05997889b2c70e5dcb8e45e5c791f2a12f0f384"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/blacktop/ipsw/releases/download/v20.08.88/ipsw_20.08.88_linux_arm64.tar.gz"
-    sha256 "61a237cb800d14f27a06ef9a59ae4a7a1ae20731f7e646e57eabfcfd124d59dc"
+    url "https://github.com/blacktop/ipsw/releases/download/v20.08.89/ipsw_20.08.89_linux_arm64.tar.gz"
+    sha256 "cb29ba99f15cdd5a629f2ed2659530fd4de6243b69df391c66ae9b60bc0cbcb6"
   end
 
   depends_on "bat"
