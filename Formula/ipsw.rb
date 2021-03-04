fc1772ea1cafc257d32bd0dc32b6d34aedb5f319
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-03 22:42:24 -0500

    Brew formula update for ipsw version v3.0.3

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,24 +5,24 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "3.0.2"
+  version "3.0.3"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.2/ipsw_3.0.2_macOS_x86_64.tar.gz"
-    sha256 "7f982ae628ce884acfda85cd5a58362ecd0fd8c6ab83b2e7a9943c1354b5368d"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.3/ipsw_3.0.3_macOS_x86_64.tar.gz"
+    sha256 "717404a0d01f0b9f0c2de544850ce61ac8c197abc7a951aa3e8b4c377368d27d"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.2/ipsw_3.0.2_macOS_arm64.tar.gz"
-    sha256 "66c288b69d4a8e81230763531041d01af1a55e4720cf9c71e562749ab782ed2d"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.3/ipsw_3.0.3_macOS_arm64.tar.gz"
+    sha256 "92b235d7381cd1563fcf0d746e06e530b74af98ed884ae4dfe97d71682aff663"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.2/ipsw_3.0.2_linux_x86_64.tar.gz"
-    sha256 "ea925f6119a54842430aa6d0e8af582d298af77d8f0b6ee832609825c5e7d465"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.3/ipsw_3.0.3_linux_x86_64.tar.gz"
+    sha256 "e01f745ead42ddcce12cfeb9cd744109ec3e631c363229617a89434e668dba3b"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.2/ipsw_3.0.2_linux_arm64.tar.gz"
-    sha256 "6e8132316f87306d0dcd421425244e0192a136d0fd7249127bffec3f3ab9e49f"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.3/ipsw_3.0.3_linux_arm64.tar.gz"
+    sha256 "7625e92462f15feb21714c662ca0406d05128ed74f3c8f20b33494971afd4d1b"
   end
 
   depends_on "bat"
