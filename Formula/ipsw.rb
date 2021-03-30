32d971043371f9a57b944823f96dcd6e9e5bba68
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-30 08:34:54 -0400

    Brew formula update for ipsw version v3.0.14

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,24 +5,24 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "3.0.13"
+  version "3.0.14"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.13/ipsw_3.0.13_macOS_x86_64.tar.gz"
-    sha256 "08c85514719a4756bce2069d3f00e815b90cfc063e667294e72f94f4771751f7"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.14/ipsw_3.0.14_macOS_x86_64.tar.gz"
+    sha256 "84354be8a42b9dace39c4c0db454b50ec5d8c9d16da55c6373d8637b2ab32999"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.13/ipsw_3.0.13_macOS_arm64.tar.gz"
-    sha256 "b815fa8bd7c2b204a8e7246e95bc0f89b91960b4555e8edb0b5d76bf6734b0ad"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.14/ipsw_3.0.14_macOS_arm64.tar.gz"
+    sha256 "e7a9b2ff9195bfff4236d778b96671170d8410833477a06d1a5d405d143dc340"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.13/ipsw_3.0.13_linux_x86_64.tar.gz"
-    sha256 "bd1749ea3b60328606a08fc34c2e73901ebe9fcb025c56bfd1db7c587f18bad8"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.14/ipsw_3.0.14_linux_x86_64.tar.gz"
+    sha256 "d650ec6dd2298202b805bf6fb8f361711feb0ff91bafa73afb2e4a712efe56fd"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/blacktop/ipsw/releases/download/v3.0.13/ipsw_3.0.13_linux_arm64.tar.gz"
-    sha256 "8ff020749876e4e248ec609957ba5e080e12759bc720c0672f267d7236fbf7eb"
+    url "https://github.com/blacktop/ipsw/releases/download/v3.0.14/ipsw_3.0.14_linux_arm64.tar.gz"
+    sha256 "00e82cc778e56f611a81e4270810191b90aa0828dcb160e8f5d141969cf7a4a3"
   end
 
   depends_on "bat"
