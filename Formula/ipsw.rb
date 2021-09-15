314f3650ab1492dc35283ab469fbab59c0e6b622
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-14 18:22:18 -0600

    Brew formula update for ipsw version v3.0.72

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,28 +5,28 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "3.0.71"
+  version "3.0.72"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.71/ipsw_3.0.71_macOS_x86_64.tar.gz"
-      sha256 "f37a00d64f806863a38de55bd8882ae26d35d9b25e0057a23ce09c552f712105"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_macOS_x86_64.tar.gz"
+      sha256 "8dd732bde02b0adaf96461015f21c5c4b0e8eef0de5c975bad4d2ac53502d110"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.71/ipsw_3.0.71_macOS_arm64.tar.gz"
-      sha256 "9486214133ff930a22139e25717150d63c6beb2bc9949447dd40a80b64a1715f"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_macOS_arm64.tar.gz"
+      sha256 "1b2557ce62049a33755fc3466ff92563604300c00ad01964f701c2fdb5c3bd08"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.71/ipsw_3.0.71_linux_x86_64.tar.gz"
-      sha256 "178d79caf993149e585fe0879db18591840a77c0d72a814dc91b0cf87e3a3c90"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_linux_x86_64.tar.gz"
+      sha256 "0f9edf2a6195c348bafac01f0e14621e7de4cdd30599eb4128d1212e2ed29a6a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.71/ipsw_3.0.71_linux_arm64.tar.gz"
-      sha256 "71f44dc85001dc3b00687108530a6f3eefd4c51586653aea2f87fb709bf16c19"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_linux_arm64.tar.gz"
+      sha256 "ed0cf316957dfde9f683693d56ced81e9e4d7e5e12f9404a8b9bc3c0cc8bcfa3"
     end
   end
 
