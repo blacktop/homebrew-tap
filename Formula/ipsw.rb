580358ab124a91c8c0f444c4614836e5c1301cce
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-17 01:32:59 -0600

    Brew formula update for ipsw version v3.0.73

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,28 +5,28 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "3.0.72"
+  version "3.0.73"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_macOS_x86_64.tar.gz"
-      sha256 "8dd732bde02b0adaf96461015f21c5c4b0e8eef0de5c975bad4d2ac53502d110"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.73/ipsw_3.0.73_macOS_x86_64.tar.gz"
+      sha256 "ee1d1aa829b886cbdf90a007c6d1e2ff9a6f3d5c553553d245a737127b232af1"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_macOS_arm64.tar.gz"
-      sha256 "1b2557ce62049a33755fc3466ff92563604300c00ad01964f701c2fdb5c3bd08"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.73/ipsw_3.0.73_macOS_arm64.tar.gz"
+      sha256 "602ce24a99daeb454fe5a05c726110bdc4979b114b67ac43c325b0c86c7d4779"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_linux_x86_64.tar.gz"
-      sha256 "0f9edf2a6195c348bafac01f0e14621e7de4cdd30599eb4128d1212e2ed29a6a"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.73/ipsw_3.0.73_linux_x86_64.tar.gz"
+      sha256 "471c1fef5f6c4ea06e6849472d51b2d649665e6aad4a041b556156875e558496"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.72/ipsw_3.0.72_linux_arm64.tar.gz"
-      sha256 "ed0cf316957dfde9f683693d56ced81e9e4d7e5e12f9404a8b9bc3c0cc8bcfa3"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.73/ipsw_3.0.73_linux_arm64.tar.gz"
+      sha256 "02c96a606b6309a5780511dc9a1a2fec2c92c46fed57dabdb7a62624baa86a58"
     end
   end
 
