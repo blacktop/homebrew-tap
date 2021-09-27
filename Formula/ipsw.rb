36d5d726fda9b3fa36d32b8457e2d9e905f43d87
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-27 01:11:22 -0600

    Brew formula update for ipsw version v3.0.83

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,28 +5,28 @@
 class Ipsw < Formula
   desc "Download and parse ipsw(s)"
   homepage "https://github.com/blacktop/ipsw"
-  version "3.0.82"
+  version "3.0.83"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.82/ipsw_3.0.82_macOS_x86_64.tar.gz"
-      sha256 "15e6d8da0a617994a19326927599dd9c5ecdea0e8524580a4bf8244c6e65a1bc"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.83/ipsw_3.0.83_macOS_x86_64.tar.gz"
+      sha256 "0c05aebdf035c9c62b610f83b1ff88a125b228d8ec6914082419f1389fe053b5"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.82/ipsw_3.0.82_macOS_arm64.tar.gz"
-      sha256 "c1df6f8dde5796c3b104fc2f6357ac9ee10d8875f75495050587627ff7b9390d"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.83/ipsw_3.0.83_macOS_arm64.tar.gz"
+      sha256 "fd7c2afd2dcedfe8672cfcd6fa5438e7007177be2214c87f1361b46b5e26ce5e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.82/ipsw_3.0.82_linux_x86_64.tar.gz"
-      sha256 "47919ddf6a0d1e986257c3c989f971220b52ecd98cf7f29f83df26654bedf06b"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.83/ipsw_3.0.83_linux_x86_64.tar.gz"
+      sha256 "04362c51c2be421c0ebca7dbe387af518fd75b4c916fcd385be2319d51efe303"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.0.82/ipsw_3.0.82_linux_arm64.tar.gz"
-      sha256 "cae6c6c6a7e78a9b51cf2dce6b2b0ed985d88326b16e9186d56279e98f9080ad"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.0.83/ipsw_3.0.83_linux_arm64.tar.gz"
+      sha256 "1ec6b89e777df6a280b16ab98da809b63d56f96db343492a398edc6e489b8a8f"
     end
   end
 
