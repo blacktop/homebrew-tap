e26f795621879191eac9b36e79ab133894b1c7cd
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-17 17:54:33 -0600

    Brew formula update for go-apfs version v1.0.9

--- Formula/apfs.rb
+++ Formula/apfs.rb
@@ -5,19 +5,13 @@
 class Apfs < Formula
   desc "MachO ARMv9-a apfsembler"
   homepage "https://github.com/blacktop/go-apfs"
-  version "1.0.8"
+  version "1.0.9"
   bottle :unneeded
   depends_on :macos
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/blacktop/go-apfs/releases/download/v1.0.8/apfs_1.0.8_macOS_arm64.tar.gz"
-      sha256 "6feae624a1ca0392ed6a0d4250a5c8bfd13477d96ff1edfb2ec4533ae57fd1f7"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/blacktop/go-apfs/releases/download/v1.0.8/apfs_1.0.8_macOS_x86_64.tar.gz"
-      sha256 "aa70262c8b5ffb27a0440b702a4d1b3154da585f9bd8b6fb75e5e84f5b11ce64"
-    end
+    url "https://github.com/blacktop/go-apfs/releases/download/v1.0.9/apfs_1.0.9_macOS_universal.tar.gz"
+    sha256 "919fff44be3a0814b665884bfd9d47c432a9e28816472335e26e3239748c478d"
   end
 
   depends_on "bat" => :optional
