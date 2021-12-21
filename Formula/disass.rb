baf131173568dd891933c192220a76b2da6623a3
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-20 17:21:19 -0700

    Brew formula update for arm64-cgo version v1.0.47

--- Formula/disass.rb
+++ Formula/disass.rb
@@ -5,12 +5,12 @@
 class Disass < Formula
   desc "MachO ARMv9-a Disassembler"
   homepage "https://github.com/blacktop/arm64-cgo"
-  version "1.0.46"
+  version "1.0.47"
   depends_on :macos
 
   on_macos do
-    url "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.46/disass_1.0.46_macOS_universal.tar.gz"
-    sha256 "fd86809db92601c714d5f0d8366142bf8be0bba59eb6e91a1b8fda440481bff5"
+    url "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.47/disass_1.0.47_macOS_universal.tar.gz"
+    sha256 "2c5852f8e111637b40b941e354e64d31c72ff13da9dc44ccde3d3efb76c7eca2"
 
     def install
       bin.install "disass"
