290256aee15a240587914ef6e52fd7b60a35eaef
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-10 20:13:35 -0700

    Brew formula update for ipsw version v3.1.62

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -12,7 +12,7 @@ class Ipsw < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/blacktop/ipsw/releases/download/v3.1.62/ipsw_macOS_arm64.tar.gz"
-      sha256 "9d3f419f6d8220b76e8c1b2839d053d01c934ba5cf1f6b2d3b5d628fc68b3d50"
+      sha256 "208da359c496f1c5f89887be2e30dd49b1986a38ed2d77a175fa5b05433da6eb"
 
       def install
         bin.install "ipsw"
@@ -24,7 +24,7 @@ class Ipsw < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/blacktop/ipsw/releases/download/v3.1.62/ipsw_macOS_x86_64.tar.gz"
-      sha256 "497cbda131b3643ff1fb4ae3ab305107f423240eccdd10c39914ae224e6d9d82"
+      sha256 "5a321a325251b14cf70160186965f042fa73d152f3dd929a9dca34a05fe53423"
 
       def install
         bin.install "ipsw"
