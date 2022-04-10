5102b103f50387c43226baf37fd0d513998cca38
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-09 23:37:34 -0600

    Brew formula update for ipsw version v3.1.94

--- Formula/ipsw.rb
+++ Formula/ipsw.rb
@@ -5,14 +5,14 @@
 class Ipsw < Formula
   desc "iOS/macOS Research Swiss Army Knife"
   homepage "https://github.com/blacktop/ipsw"
-  version "3.1.93"
+  version "3.1.94"
   license "MIT"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.1.93/ipsw_3.1.93_macOS_arm64.tar.gz"
-      sha256 "6f337e9e168a1ed4dfb00ffa35d9d5cfdf3ac5bce6f7dadec0b579d4c8065d7e"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.1.94/ipsw_3.1.94_macOS_arm64.tar.gz"
+      sha256 "ed5c9f4b6a2050b1ee0f7aa4373055f7d7a8a820a2351eb8af59dd7da6b224b0"
 
       def install
         bin.install "ipsw"
@@ -23,8 +23,8 @@ class Ipsw < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/blacktop/ipsw/releases/download/v3.1.93/ipsw_3.1.93_macOS_x86_64.tar.gz"
-      sha256 "f8f46ce2d94576eb4bb3dcf1ea3c9249b5af21e61c13a843f44ccf19da432c3e"
+      url "https://github.com/blacktop/ipsw/releases/download/v3.1.94/ipsw_3.1.94_macOS_x86_64.tar.gz"
+      sha256 "dff73be48a4d90f1f2ef8f9fc42c854671853a746ccf8668a05dd9e203df7a2e"
 
       def install
         bin.install "ipsw"
