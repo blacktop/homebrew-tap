30a14295e44e6cf40b3a0401c5587968cdc0cfdc
blacktop <blacktop@users.noreply.github.com>
2022-08-01 19:46:47 -0600

    Update go.rb

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.18.4.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.18.4.src.tar.gz"
-  sha256 "4525aa6b0e3cecb57845f4060a7075aafc9ab752bb7b6b4cf8a212d43078e1e4"
+  url "https://go.dev/dl/go1.18.5.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.18.5.src.tar.gz"
+  sha256 "9920d3306a1ac536cdd2c796d6cb3c54bc559c226fc3cc39c32f1e0bd7f50d2a"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
 
