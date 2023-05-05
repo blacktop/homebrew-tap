f47e470c73d3583679d0af56f9c67366acf4fadb
blacktop <blacktop@users.noreply.github.com>
2023-05-04 20:22:59 -0600

    Update frida-tools.rb

--- Formula/frida-tools.rb
+++ Formula/frida-tools.rb
@@ -3,8 +3,8 @@ class FridaTools < Formula
 
   desc "CLI tools for Frida"
   homepage "https://www.frida.re/"
-  url "https://files.pythonhosted.org/packages/78/fe/e73984036a07df63ed9c0c83a12037f59c7dfbc9fe3e8bbcf4b1a67b9c53/frida-tools-10.5.4.tar.gz"
-  sha256 "8f96826ccb30d14890eff26f07ef94454f33fac729afe8bd6e5daccbb2f6e2c0"
+  url "https://files.pythonhosted.org/packages/1b/0c/885cb17beeae600897794d668541102995378a21e4a2d938b2c87cb6e1be/frida-tools-12.1.2.tar.gz"
+  sha256 "f52943ca9d5f2ceccbaaeeacc55a98da3c04cd08eb9dbcdf417211a32be23c96"
   license "MIT"
   revision 3
 
@@ -16,23 +16,23 @@ class FridaTools < Formula
   depends_on "python@3.10"
 
   resource "colorama" do
-    url "https://files.pythonhosted.org/packages/1f/bb/5d3246097ab77fa083a61bd8d3d527b7ae063c7d8e8671b1cf8c4ec10cbe/colorama-0.4.4.tar.gz"
-    sha256 "5941b2b48a20143d2267e95b1c2a7603ce057ee39fd88e7329b0c292aa16869b"
+    url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
+    sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
   end
 
   resource "frida" do
-    url "https://files.pythonhosted.org/packages/c6/0e/9b837472e1dd866ef002a6bf5f2dde42b0c4f0bfcf8f5ab80797a148025e/frida-15.1.17.tar.gz"
-    sha256 "7f2a8e41864648e3bab86d24a52f8ac1b451b3e6de069b6ce311e618bd59b758"
+    url "https://files.pythonhosted.org/packages/0b/5f/68e3f8fbe455ea305235e4124d3fec8009f56b04e252b1f920e7b47f986a/frida-16.0.19.tar.gz"
+    sha256 "ae29088e39fdc00f152ff4adff62494dcb9e8a69feabf5116edf65c3ffa76a56"
   end
 
   resource "prompt_toolkit" do
-    url "https://files.pythonhosted.org/packages/37/34/c34c376882305c5051ed7f086daf07e68563d284015839bfb74d6e61d402/prompt_toolkit-3.0.28.tar.gz"
-    sha256 "9f1cd16b1e86c2968f2519d7fb31dd9d669916f515612c269d14e9ed52b51650"
+    url "https://files.pythonhosted.org/packages/4b/bb/75cdcd356f57d17b295aba121494c2333d26bfff1a837e6199b8b83c415a/prompt_toolkit-3.0.38.tar.gz"
+    sha256 "23ac5d50538a9a38c8bde05fecb47d0b403ecd0662857a86f886f798563d5b9b"
   end
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
-    sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
+    url "https://files.pythonhosted.org/packages/89/6b/2114e54b290824197006e41be3f9bbe1a26e9c39d1f5fa20a6d62945a0b3/Pygments-2.15.1.tar.gz"
+    sha256 "8ace4d3c1dd481894b2005f560ead0f9f19ee64fe983366be1a21e171d12775c"
   end
 
   resource "six" do
@@ -41,8 +41,8 @@ class FridaTools < Formula
   end
 
   resource "wcwidth" do
-    url "https://files.pythonhosted.org/packages/89/38/459b727c381504f361832b9e5ace19966de1a235d73cdbdea91c771a1155/wcwidth-0.2.5.tar.gz"
-    sha256 "c4d647b99872929fdb7bdcaa4fbe7f01413ed3d98077df798530e5b04f116c83"
+    url "https://files.pythonhosted.org/packages/5e/5f/1e4bd82a9cc1f17b2c2361a2d876d4c38973a997003ba5eb400e8a932b6c/wcwidth-0.2.6.tar.gz"
+    sha256 "a5220780a404dbe3353789870978e472cfe477761f06ee55077256e509b156d0"
   end
 
   def install
