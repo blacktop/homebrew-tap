bde2bd5bcb68a533f5269a8a8249b7bcd461d2d6
blacktop <blacktop@users.noreply.github.com>
2023-12-24 21:46:58 -0700

    Update ghidra-app.rb

--- Casks/ghidra-app.rb
+++ Casks/ghidra-app.rb
@@ -2,7 +2,8 @@ cask "ghidra-app" do
   version "11.0.0"
   sha256 "74d1c5db9b38d1d627e1d20769ac429810bbbd7ed02d33e40657f31b3620aad7"
 
-  url "https://github.com/blacktop/ghidra-apple-silicon/releases/download/v11.0/Ghidra_11.0.zip"
+  url "https://github.com/blacktop/ghidra-apple-silicon/releases/download/v11.0/Ghidra_11.0.zip",
+      verified: "github.com/blacktop/ghidra-apple-silicon/"
   name "Ghidra.app (Apple Silicon)"
   desc "Software reverse engineering (SRE) suite of tools"
   homepage "https://github.com/blacktop/ghidra-apple-silicon"
