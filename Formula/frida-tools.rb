95e508f54a28bd2d7975c26f649dd4e100624c9a
blacktop <blacktop@users.noreply.github.com>
2024-10-14 13:57:01 -0600

    Update frida-tools.rb

--- Formula/frida-tools.rb
+++ Formula/frida-tools.rb
@@ -15,37 +15,33 @@ class FridaTools < Formula
 
   depends_on "python@3.13"
 
-  # resource "colorama" do
-  #   url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
-  #   sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
-  # end
+  resource "colorama" do
+    url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
+    sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
+  end
 
-  # resource "frida" do
-  #   url "https://files.pythonhosted.org/packages/2b/b5/7f32692b66a69bf255b1d6cc6c04e1b4b1f40d6060d58e94f813acd88a3b/frida-16.5.6.tar.gz"
-  #   sha256 "87b5c2df71d5599358247f2666ddc051c9f18c14366470dcb19e98683b3d69a6"
-  # end
+  resource "frida" do
+    url "https://files.pythonhosted.org/packages/2b/b5/7f32692b66a69bf255b1d6cc6c04e1b4b1f40d6060d58e94f813acd88a3b/frida-16.5.6.tar.gz"
+    sha256 "87b5c2df71d5599358247f2666ddc051c9f18c14366470dcb19e98683b3d69a6"
+  end
 
-  # resource "prompt_toolkit" do
-  #   url "https://files.pythonhosted.org/packages/2d/4f/feb5e137aff82f7c7f3248267b97451da3644f6cdc218edfe549fb354127/prompt_toolkit-3.0.48.tar.gz"
-  #   sha256 "d6623ab0477a80df74e646bdbc93621143f5caf104206aa29294d53de1a03d90"
-  # end
+  resource "prompt_toolkit" do
+    url "https://files.pythonhosted.org/packages/2d/4f/feb5e137aff82f7c7f3248267b97451da3644f6cdc218edfe549fb354127/prompt_toolkit-3.0.48.tar.gz"
+    sha256 "d6623ab0477a80df74e646bdbc93621143f5caf104206aa29294d53de1a03d90"
+  end
 
-  # resource "Pygments" do
-  #   url "https://files.pythonhosted.org/packages/8e/62/8336eff65bcbc8e4cb5d05b55faf041285951b6e80f33e2bff2024788f31/pygments-2.18.0.tar.gz"
-  #   sha256 "786ff802f32e91311bff3889f6e9a86e81505fe99f2735bb6d60ae0c5004f199"
-  # end
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/8e/62/8336eff65bcbc8e4cb5d05b55faf041285951b6e80f33e2bff2024788f31/pygments-2.18.0.tar.gz"
+    sha256 "786ff802f32e91311bff3889f6e9a86e81505fe99f2735bb6d60ae0c5004f199"
+  end
 
-  # resource "websockets" do
-  #   url "https://files.pythonhosted.org/packages/e2/73/9223dbc7be3dcaf2a7bbf756c351ec8da04b1fa573edaf545b95f6b0c7fd/websockets-13.1.tar.gz"
-  #   sha256 "a3b3366087c1bc0a2795111edcadddb8b3b59509d5db5d7ea3fdd69f954a8878"
-  # end
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/e2/73/9223dbc7be3dcaf2a7bbf756c351ec8da04b1fa573edaf545b95f6b0c7fd/websockets-13.1.tar.gz"
+    sha256 "a3b3366087c1bc0a2795111edcadddb8b3b59509d5db5d7ea3fdd69f954a8878"
+  end
 
   def install
-    # virtualenv_install_with_resources
-    # pkgshare.install buildpath/"examples"
-    # venv = virtualenv_create(libexec)
-    # venv.pip_install_and_link buildpath
-    system Formula["python@3.13"].opt_bin/"python3.13", *Language::Python.setup_install_args(libexec)
+    virtualenv_install_with_resources
   end
 
   test do
