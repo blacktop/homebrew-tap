5cd6109af84accfc5b76aa931615dd931a1bfea5
blacktop <blacktop@users.noreply.github.com>
2025-04-15 15:37:39 -0600

    Update ghidra-app.rb

--- Casks/ghidra-app.rb
+++ Casks/ghidra-app.rb
@@ -1,6 +1,6 @@
 cask "ghidra-app" do
-  version "11.3.1"
-  sha256 "0ea6cb64e8a59d912bca15e8aa6e61fb855fdf40197ea098c93e8b0de7d57f6b"
+  version "11.3.2"
+  sha256 "84c761d181a974739b95e068ea6dbfdee5c2657889148e84d4b52d9c1e136320"
 
   url "https://github.com/blacktop/ghidra-app/releases/download/v#{version}/Ghidra_#{version}.zip"
   name "Ghidra.app"
