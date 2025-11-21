c38d110c219532f02fffe41f6b7220f4c87ae886
blacktop <blacktop@users.noreply.github.com>
2025-11-20 22:31:45 -0700

    Update pdf.rb

--- Casks/pdf.rb
+++ Casks/pdf.rb
@@ -1,6 +1,6 @@
 cask "pdf" do
-  version "0.7.1"
-  sha256 "214ed380115575ad3cd545d276614eb7fab45acf7791e5cdc9ac3291882e1888"
+  version "0.7.3"
+  sha256 "6d748a02f89c56e3ea3d3d41d06efd26abf56797f230c028d42b8f78285d6c70"
 
   url "https://github.com/blacktop/pdf/releases/download/v#{version}/pdf-#{version}-darwin-ARM64.tar.gz",
       verified: "github.com/blacktop/pdf/"
