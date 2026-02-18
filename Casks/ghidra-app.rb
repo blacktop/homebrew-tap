5fd656ab3073582e77d1d38ca4464950dc43da87
blacktop <blacktop@users.noreply.github.com>
2026-02-17 18:20:41 -0700

    Update ghidra-app.rb

--- Casks/ghidra-app.rb
+++ Casks/ghidra-app.rb
@@ -2,7 +2,7 @@ cask "ghidra-app" do
   version "12.0.3,1"
   sha256 "87c301f312a30d434f4a07f2789d148247854fef1c71dea51906e9f6f5e395a8"
 
-  url "https://github.com/blacktop/ghidra-app/releases/download/v#{version}/Ghidra_#{version}.zip"
+  url "https://github.com/blacktop/ghidra-app/releases/download/v#{version.before_comma}/Ghidra_#{version.before_comma}.zip"
   name "Ghidra.app"
   desc "Software reverse engineering (SRE) suite of tools"
   homepage "https://github.com/blacktop/ghidra-app"
